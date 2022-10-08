42c6ad6f12c40d8fe4b9e42253a82dac897dde53
Anuradha Sampath Hewa Siribaddana <anusampath9470@gmail.com>
2022-10-08 10:56:53 +0530

    Test auth needed

--- BeginnerApp/BeginnerApp/Controllers/HomeController.cs
+++ BeginnerApp/BeginnerApp/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace BeginnerApp.Controllers
 {
+    // Home controller
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
