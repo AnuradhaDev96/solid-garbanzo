93e34cf938c000a785f8c10b59356962089b35a8
Anuradha Sampath Hewa Siribaddana <anusampath9470@gmail.com>
2022-10-09 15:34:13 +0530

    Razor integration done

--- BeginnerApp/BeginnerApp/Controllers/HomeController.cs
+++ BeginnerApp/BeginnerApp/Controllers/HomeController.cs
@@ -34,5 +34,6 @@ namespace BeginnerApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
-}
+}                   
