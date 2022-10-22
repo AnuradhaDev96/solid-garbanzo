a325975f419e611137f67e07839567a0737f6633
AnuradhaDev96 <anusampath9470@gmail.com>
2022-10-22 22:52:03 +0530

    Edit, Delete and Alert messages done

--- BeginnerApp/BeginnerApp/Models/Category.cs
+++ BeginnerApp/BeginnerApp/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ namespace BeginnerApp.Models
 
         [Required]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]        
+        [Range(1, 100)] // Can specify an error message also
         public int DisplayOrder { get; set; }
 
         // with default value
